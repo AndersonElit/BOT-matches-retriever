f1f199cd8f54adef8d7563a4c9f9dd2443f9fc71
AndersonElit <rodriarri@gmail.com>
2020-10-23 20:58:37 -0500

    dynamic int array

--- c_functions/yearsTemps.c
+++ c_functions/yearsTemps.c
@@ -1,48 +1,7 @@
 #include<stdio.h>
 #include<string.h>
-#include "storage.h"
-
-/*initialize empty array*/
-void Array_Init(Array *array) {
-
-    int *int_pointer;
-    int_pointer = (int *)malloc(sizeof(int));
-
-    if ( int_pointer == NULL ) {
-        printf("Unable to allocate memory, exiting.\n");
-        free(int_pointer);
-        exit(0);
-    } else {
-        array->array = int_pointer;
-        array->size = 0;
-    }
-
-}
-
-void Array_Add(Array *array, int item) {
-
-    int *int_pointer;
-    array->size += 1;
-    int_pointer = (int *)realloc(array->array, array->size * sizeof(int));
-
-    if(int_pointer == NULL) {
-        printf("Unable to reallocate memory, exiting.\n");
-        free(int_pointer);
-        exit(0);
-    } else {
-        array->array = int_pointer;
-        array->array[array->size-1] = item;
-    }
-
-}
-
-void Array_Free(Array *array) {
-
-    free(array->array);
-    array->array = NULL;
-    array->size = 0;
-
-}
+//#include "storage.h"
+#include "dynamic_int_arr.c"
 
 /*
 
